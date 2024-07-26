946f7dc098b6b10afb8202470f39fb317c7a99f0
C-ON <vmfpsel@gmail.com>
2024-07-26 16:32:17 +0900

    join member form update

--- frontend/src/pages/Login/SignUp/SignUp.jsx
+++ frontend/src/pages/Login/SignUp/SignUp.jsx
@@ -2,94 +2,44 @@ import { useState } from "react";
 import styles from "./SignUp.module.css";
 import axios from "axios";
 import { validateUserId, validatePassword } from '../../../commons/common';
+import {SignUpSub1} from "./SignUpSub/SignUpSub1";
+import {SignUpSub2} from "./SignUpSub/SignUpSub2";
+import {SignUpSub3} from "./SignUpSub/SignUpSub3";
 
-export const SignUp = () => {
-  const [ idSave, setIdSave ] = useState("");
-  const [idCheck, setIdCheck] = useState(false);
+export const SignUp = ({ signUpPage }) => {
 
-  const [ pwCheck, setPwCheck ] = useState(false);
-  const [ pwSave, setPwSave ] = useState({pw:"", pwCheck:""});
-  const handleIdCheck = async (e) => {
-    setIdSave(e.target.value);
-
-    // 정규 표현식
-    if(validateUserId(idSave)) setIdCheck(true);
-    else setIdCheck(false);
-
-    // const response = await axios.post("http://192.168.1.7/member", e.target.value);
-    // if(response.status === 200 && response.data === "OK"){
-    //   if(validateUserId(idSave)) setIdCheck(true);
-    //   else setIdCheck(false);
-    // } else {
-    //   setIdCheck(false);
-    // }
-
+  const defaultData = {
+    emp_id: "",
+    emp_pw: "",
+    emp_name: "",
+    emp_email: "",
+    emp_birth: "",
+    emp_gender: "",
+    emp_tel: "",
+    emp_address: "",
+    pw_check: ""
   }
+  const [sendData, setSendData] = useState(defaultData);
 
-  const handlePwChck = (e) => {
-    let { name, value } = e.target
-    setPwSave(prev => {
-      let data = {...prev, [name]: value}
-      if(data.pw === data.pwCheck) {
-        if(validatePassword(data.pw)) setPwCheck(true);
-        else setPwCheck(false);
-      }
-      else setPwCheck(false);
-      return data;
-    });
-
+  const defaultCheckData = {
+    emp_id: false,
+    emp_pw: false,
+    emp_name: false,
+    emp_email: false,
+    emp_birth: false,
+    emp_gender: false,
+    emp_tel: false,
+    emp_address: false
   }
+  const [checkData, setCheckData] = useState(defaultCheckData);
+
+  // 회원가입 직전에 delete sendData.pw_check 해야됨
 
   return (
     <div className={styles.container}>
-      <div className={styles.title}>
-        <h1>Join Employee!</h1>
-      </div>
-      <div className={styles.joinForm}>
-        <div className={styles.row}>
-          <span>아이디</span>
-        </div>
-        <div className={styles.row}>
-          <input type="text" name="id" onChange={handleIdCheck} placeholder="6~12자리로 작성해주세요!"/>
-        </div>
-
-        { /* ID empty check */
-          idSave !== "" ?
-            <div className={styles.row}>
-              { /* ID Check */
-                idCheck ?
-                  <p style={{color: "green"}}>사용 가능한 아이디 입니다.</p>
-                  :
-                  <p style={{color: "red"}}>사용할 수 없는 아이디 입니다.</p>
-              }
-            </div>
-            : <></>
-        }
-
-        <div className={styles.row}>
-          <span>비밀번호</span>
-        </div>
-        <div className={styles.row}>
-          <input type="text" name="pw" onChange={handlePwChck} placeholder="8~20자리로 입력하세요"/>
-        </div>
-        <div className={styles.row}>
-          <input type="text" name="pwCheck" onChange={ handlePwChck } placeholder="8~20자리로 한번더 입력하세요"/>
-        </div>
-
-        { /* Password empty check */
-          pwSave.pw !== "" || pwSave.pwCheck !== "" ?
-            <div className={styles.row}>
-              {pwCheck ?
-                <p style={{color: "green"}}>비밀번호 일치</p>
-                :
-                <p style={{color: "red"}}>비밀번호 불일치</p>
-              }
-            </div>
-            :
-            <></>
-        }
-
-      </div>
+      {signUpPage === 1 && <SignUpSub1 sendData={sendData} checkData={checkData} setSendData={setSendData} setCheckData={setCheckData} />}
+      {signUpPage === 2 && <SignUpSub2 sendData={sendData} checkData={checkData} setSendData={setSendData} setCheckData={setCheckData} />}
+      {signUpPage === 3 && <SignUpSub3 sendData={sendData} checkData={checkData} setSendData={setSendData} setCheckData={setCheckData} />}
     </div>
   );
 };
