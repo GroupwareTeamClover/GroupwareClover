41a716de869d26632d0c9d00bbd03d216b91e5a7
Jin <sebgokti4276@naver.com>
2024-07-29 10:36:32 +0900

    환경변수 설정

--- frontend/src/commons/config.js
+++ frontend/src/commons/config.js
@@ -1,4 +1,17 @@
 // root 폴더에 있는 env 파일 안 REACT_APP_BASE_URL 설정을 불러오는 명령어.
 // env_example 파일 작성 예정
-export const BaseUrl = process.env.REACT_APP_BASE_URL;
+export const BaseUrl = () => {
+    return 'http://192.168.1.5';   
+}
+
+// export const BaseUrl = process.env.REACT_APP_API_URL;
+
 if(!BaseUrl) throw new Error('REACT_APP_BASE_URL가 없음');
+
+
+
+// export const baseUrl = () => {
+//     return 'http://192.168.1.5'; // replace with your backend server URL
+// }
+// frontend 최상단package.json 있는 구역에서 .env 파일 생성 후 
+// REACT_APP_BASE_URL = http://192.168.45.61(본인 아이피) 생성해놓으면 됨.
