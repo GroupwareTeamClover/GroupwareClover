eece5cab829fcd2238e5f272ec93608a60bfb4ed
Sion-Noh <vmfpsel@gmail.com>
2024-08-02 10:18:00 +0900

    캘린더 UI 작업 중... Fullcalendar 라이브러리 추가

--- frontend/src/pages/Home/Content/Calendar/Calendar.jsx
+++ frontend/src/pages/Home/Content/Calendar/Calendar.jsx
@@ -2,20 +2,95 @@ import styles from './Calendar.module.css'
 import './Calendar.css';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import {useEffect, useState} from "react";
 
 export const Calendar = () => {
 
+  /** Full calendar plugin **/
+  const plugin = [
+    dayGridPlugin,      // 월간 달력
+    timeGridPlugin,     // 주간, 일간 달력
+    interactionPlugin   // 이벤트를 위한 플러그인
+  ];
+
+  /** 캘린더에 표시될 이벤트 상태 **/
+  const [schedule, setSchedule] = useState([
+    {title: "캘린더 테스트", date: "2024-08-23"},
+    {title: "합니다", date: "2024-08-24"}
+  ]);
+
+  useEffect(() => {
+    // 캘린더 페이지 들어왔을 때 스케줄 목록 바인딩 해야됨
+
+  }, []);
+
   return (
     <div className={styles.container}>
-      <div className={styles.calendar}>
-        <FullCalendar
-          plugins={[ dayGridPlugin ]}
-          initialView="dayGridMonth"
-          events={[
-            { title: "캘린더", date: "2024-07-23"},
-            { title: "실행됨", date: "2024-07-24"},
-          ]}
-        />
+      <div className={styles.form}>
+        <div className={styles.subMenu}>
+
+        </div>
+        <div className={styles.calendar}>
+          <FullCalendar
+            plugins={plugin}
+            initialView="dayGridMonth"
+
+            locale={'ko'}
+
+            headerToolbar={{
+              left: "prev next",
+              center: "title",
+              right: "dayGridMonth timeGridWeek timeGridDay today"
+            }}
+
+            buttonText={{
+              today: "Today",
+              month: "Month",
+              week: "Week",
+              day: "Day"
+            }}
+
+            events={schedule}
+
+          />
+          {/*
+            [ event = 일정 ]
+            events={scheduleData}     // 달력에 표시 될 이벤트
+            eventClick={eventClick}   // 이벤트 클릭시
+            eventChange={eventChange} // 이벤트 drop 혹은 resize 될 때
+            editable={true}           // 사용자의 수정 가능 여부 (이벤트 추가/수정, 드래그 앤 드롭 활성화)
+            selectable={true}         // 사용자의 날짜 선택 여부
+            selectMirror={true}       // 사용자의 시간 선택시 time 표시 여부
+            select={select}           // 날짜가 선택 될 때
+            weekends={true}           // 주말 표시 여부
+            dayMaxEvents={true}       // 하루에 표시 될 최대 이벤트 수 true = 셀의 높이
+            navLinks={true}           // 달력의 날짜 클릭시 일간 스케쥴로 이동
+            navLinkHint={"클릭시 해당 날짜로 이동합니다."} // 날짜에 호버시 힌트 문구
+
+            // eventContent={fn(): node {} || true}   // 일정 커스텀
+            eventsSet={function () {
+              console.log("eventsSet");
+            }}
+
+            // 추가시 로직
+            eventAdd={function () {
+              console.log("eventAdd");
+            }}
+
+            // 드롭시 로직
+            eventDrop={function () {
+              console.log("eventDrop");
+            }}
+
+            // 제거시 로직
+            eventRemove={function () {
+              console.log("eventRemove");
+            }}
+
+          */}
+        </div>
       </div>
     </div>
   );
