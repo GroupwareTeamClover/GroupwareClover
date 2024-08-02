2f96c3fbc0aca220458df197ece9b28006f3afef
SaeMiPark <samming3013@gmail.com>
2024-08-02 22:55:03 +0900

    인서트 전까지 완성

--- frontend/src/pages/Home/Content/Approval/WriteForm/WriteForm.jsx
+++ frontend/src/pages/Home/Content/Approval/WriteForm/WriteForm.jsx
@@ -4,13 +4,17 @@ import { Business } from '../components/ApprovalForms/Business/Business';
 import { Dayoff } from '../components/ApprovalForms/Dayoff/Dayoff';
 import { BsPencilSquare, BsDownload } from "react-icons/bs";
 import { MdOutlineCancel } from "react-icons/md";
+import { MdEmergency } from "react-icons/md";
+import { useState, useEffect } from 'react';
+import { useMemberStore } from "../../../../../store/store";
+
 
 export const WriteForm = () => {
     const location = useLocation();
     const state=location.state;
-    console.log(state.type);
-    console.log(state.docinfo);
-    console.log(state.empinfo);
+    // console.log(state.type);
+    // console.log(state.docinfo);
+    // console.log(state.empinfo);
 
     //맵핑해서 하나로 움직이게 하기
     const formConfig = {
@@ -21,21 +25,87 @@ export const WriteForm = () => {
 
     const FormComponent = formConfig[state.type] || formConfig.invalid;
 
+    //세션정보
+    const {sessionData} = useMemberStore();
+
+    // 현재 날짜 마운트 시 한 번만
+    const [currentDate, setCurrentDate] = useState('');
+    useEffect(() => {
+       setCurrentDate(new Date().toLocaleDateString());
+    }, []);
+    
+    const [isInsert, setIsInsert] = useState(false);
+
+    const handleInsert= () =>{
+        setIsInsert(true);
+    }
+
+    // 긴급여부 체크박스 상태
+    const [isEmergency, setIsEmergency] = useState(false);
+    const handleEmergencyChange = (e) => {
+       setIsEmergency(e.target.checked);
+    }
+
+
     return (
         <div className={styles.container}>
             <div className={styles.header}>
                 <h3 className={styles.headerText}>{state.type}</h3>
             </div>
             <div className={styles.menu}>
-                <span className={styles.menuspancenter}><BsPencilSquare size={19}/>결재요청</span>
-                <span className={styles.menuspancenter}><BsDownload size={19}/>임시저장</span>
-                <span className={styles.menuspancenter}><MdOutlineCancel size={20}/>취소</span>
+                <span className={styles.menuspancenter}><BsPencilSquare size={19} onClick={handleInsert} className={styles.menuright}/>결재요청</span>
+                <span className={styles.menuspancenter}><BsDownload size={19} className={styles.menuright}/>임시저장</span>
+                <span className={styles.menuspancenter}><MdOutlineCancel size={20} className={styles.menuright}/>취소</span>
+                <span className={styles.menuspancenter}><MdEmergency className={styles.menuright}/>긴급여부<input type="checkbox" className={styles.inputcheck} onChange={handleEmergencyChange}></input></span>
             </div>
             <div className={styles.detail}>
                 {/* 왼쪽 */}
                 <div className={styles.left}>
-                    <div className={styles.form}>
-                        <FormComponent type={state.type} docinfo={state.docinfo} empinfo={state.empinfo}/>
+                    <div className={styles.test}>
+                    <div className={styles.leftcontainer}>
+                    <div className={styles.insideheader}>
+                        <span><h2>{state.type}</h2></span>
+                    </div>
+                    <div className={styles.bigcontent}>
+                        <div className={styles.info}> 
+                            <div className={styles.docInfo}>
+                                <div className={styles.tablebox}>
+                                    <div className={styles.row1}>
+                                    <div className={styles.row1col1}>기안자</div>
+                                    <div className={styles.row1col2}>{sessionData.empName}</div>
+                                    </div>
+                                    <div className={styles.row2}>
+                                    <div className={styles.row2col1}>소속</div>
+                                    <div className={styles.row2col2}>값</div>
+                                    </div>
+                                    <div className={styles.row3}>
+                                    <div className={styles.row3col1}>기안일</div>
+                                    <div className={styles.row3col2}>{currentDate}</div>
+                                    </div>
+                                    <div className={styles.row4}>
+                                    <div className={styles.row4col1}>문서번호</div>
+                                    <div className={styles.row4col2}></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.lineEmptyBox}></div>
+                                {state.empinfo.apvchoice && state.empinfo.apvchoice.length > 0 && (
+                                        <div className={styles.apvlineInfo}>
+                                            {state.empinfo.apvchoice.map((line, index) => (
+                                                <div key={index} className={styles.tablelbox2}>
+                                                    <div className={styles.role}><span className={styles.roleText}>{line.role}</span></div>
+                                                    <div className={styles.name}><span className={styles.nameText}>{line.name}</span></div>
+                                                    <div className={styles.docNumber}></div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    )}
+                        </div>
+                        <div className={styles.form}>
+                            <FormComponent type={state.type} docinfo={state.docinfo} empinfo={state.empinfo} isInsert={isInsert}/> 
+                        </div>
+                    </div> 
+                    </div>
                     </div>
                 </div>
                 {/* 오른쪽 */}
@@ -44,3 +114,4 @@ export const WriteForm = () => {
         </div>
     );
 };
+
--- frontend/src/pages/Home/Content/Approval/components/ApprovalForms/Business/Business.jsx
+++ frontend/src/pages/Home/Content/Approval/components/ApprovalForms/Business/Business.jsx
@@ -1,9 +1,12 @@
 import styles from './Business.module.css'
 import { BsChevronDoubleLeft } from 'react-icons/bs';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useMemberStore } from '../../../../../../../store/store';
+import  WebEditor  from '../../../../../../../components/WebEditor/WebEditor';
+import axios from 'axios';
+import { BaseUrl } from '../../../../../../../commons/config';
 
-export const Business =({type, docinfo, empinfo})=>{
+export const Business =({type, docinfo, empinfo, isInsert})=>{
    console.log(empinfo);
     // lineinfo의 apvchoice 배열을 순회하며 객체 정보 출력
     // lineinfo.apvchoice?.map((line, index) => {
@@ -11,101 +14,71 @@ export const Business =({type, docinfo, empinfo})=>{
     //     console.log(line.name);
     //     console.log(line.role);
     // });
+    const editorRef = useRef();
 
-    //세션정보
-    const {sessionData} = useMemberStore();
+    //날짜, 제목
+    const [date, setDate]=useState();
+    const [title, setTitle]=useState();
 
-    //현재시간 마운트 시 한번만
-    const [currentTime, setCurrentTime] = useState('');
-    useEffect(() => {
-      setCurrentTime(new Date().toLocaleTimeString());
-    }, []); 
+    const handleDateChange=(e)=>{
+        setDate(e.target.value);
+    }
 
-    //insert axios
+    const handleTitleChange=(e)=>{
+        setTitle(e.target.value)
+    }
+
+    // <handleContentChange> - 에디터 내용 변경 시 호출할 onChange함수
+    const [content, setContent] = useState();
+    const handleContentChange = () => {
+        setContent(editorRef.current.getInstance().getHTML());
+    }
+
+    //dto로 쉽게 만들 수 있도록 만들자
+    const [business, setBusiness]=useState({});
+    //합치기
+    useEffect(()=>{
+        setBusiness()
+    },[date, title, content])
+   
+
+    //insert
+    useEffect(()=>{
+        if(isInsert){
+            axios.post(`${BaseUrl}/approval/document/business`,business).then((rest)=>{
+
+            }).catch((err)=>{
+                alert("요청 실패")
+            })
+        }
+
+    }, [isInsert])
+
     
+
+
+
     return(
         <div className={styles.container}>
-            <div className={styles.header}>
-                <span><h2>{type}</h2></span>
-            </div>
-            <div className={styles.bigcontent}>
-                {/* 내용 첫번째 줄 */}
-                <div className={styles.info}> 
-                    <div className={styles.docInfo}>
-                        <div className={styles.tablebox}>
-                            <div className={styles.row1}>
-                            <div className={styles.row1col1}>기안자</div>
-                            <div className={styles.row1col2}>{sessionData.empName}</div>
-                            </div>
-                            <div className={styles.row2}>
-                            <div className={styles.row2col1}>소속</div>
-                            <div className={styles.row2col2}>값</div>
-                            </div>
-                            <div className={styles.row3}>
-                            <div className={styles.row3col1}>기안일</div>
-                            <div className={styles.row3col2}>{currentTime}</div>
-                            </div>
-                            <div className={styles.row4}>
-                            <div className={styles.row4col1}>문서번호</div>
-                            <div className={styles.row4col2}></div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <div className={styles.apvlineInfo}> */}
-                        {empinfo.apvchoice && empinfo.apvchoice.length > 0 && (
-                                <div className={styles.apvlineInfo}>
-                                    {empinfo.apvchoice.map((line, index) => (
-                                        <div key={index} className={styles.tablelbox2}>
-                                            <div className={styles.role}><span className={styles.roleText}>{line.role}</span></div>
-                                            <div className={styles.name}><span className={styles.nameText}>{line.name}</span></div>
-                                            <div className={styles.docNumber}>결재상태예정</div>
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                    {/* </div> */}
-                </div>
-                {/* 내용 두번째 줄 */}
-                <div className={styles.content}>
-                    <div className={styles.datatitle}>
-                        <div className={styles.date}>
-                            <div className={styles.name}>시행일자</div>
-                            <div className={styles.value}>
-                                <input type='date'></input>
-                            </div>
-
-                        </div>
-                        <div className={styles.title}>
-                            <div className={styles.name}>제목</div>
-                            <div className={styles.value}>
-                                <input type='text'></input>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.detail}>
-
+            <div className={styles.datatitle}>
+                <div className={styles.date}>
+                    <div className={styles.name}>시행일자</div>
+                    <div className={styles.value}>
+                        <input type='date' className={styles.inputdate} onChange={handleDateChange}></input>
                     </div>
                 </div>
-            </div>
+                <div className={styles.title}>
+                    <div className={styles.name}>제목</div>
+                        <div className={styles.value}>
+                            <input type='text' className={styles.inputtitle} placeholder='제목을 입력하세요.' onChange={handleTitleChange}></input>
+                        </div>
+                    </div>
+                </div>
+            <div className={styles.editerContainer}>
+                <WebEditor editorRef={editorRef} handleContentChange={handleContentChange} height="100%" defaultContent=""/>
+            </div>                     
         </div>
+    
     )
 }
 
-
-{/* docinfo내용
-    <div className={styles.row1}>
-<div className={styles.row1col1}>기안자</div>
-<div className={styles.row1col2}>값</div>
-</div>
-<div className={styles.row2}>
-<div className={styles.row2col1}>소속</div>
-<div className={styles.row2col2}>값</div>
-</div>
-<div className={styles.row3}>
-<div className={styles.row3col1}>기안일</div>
-<div className={styles.row3col2}>값</div>
-</div>
-<div className={styles.row4}>
-<div className={styles.row4col1}>문서번호</div>
-<div className={styles.row4col2}>값</div>
-</div> */}
