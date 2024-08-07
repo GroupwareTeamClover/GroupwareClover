31d86236e2efe4e0d6c0f4bba156d7f708e90ec9
Sion-Noh <vmfpsel@gmail.com>
2024-08-07 18:35:14 +0900

    로그인, 회원가입 UI 변경 및 employee 테이블 address 삭제

--- frontend/src/pages/Login/SignUp/SignUpSub/SignUpSub2.jsx
+++ frontend/src/pages/Login/SignUp/SignUpSub/SignUpSub2.jsx
@@ -1,38 +1,22 @@
 import styles from './SignUpSub.module.css'
-import {validateName, validateBirth, validateGender, validateEmail} from '../../../../commons/common'
+import {sendEmail, validateEmail, validatePhone} from '../../../../commons/common'
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {BaseUrl} from "../../../../commons/config";
 
-export const SignUpSub2 = ({ sendData, checkData,  setSendData, setCheckData }) => {
+export const SignUpSub2 = ({ sendData, checkData,  setSendData, setCheckData, setSignUpState }) => {
 
+  const keys = ["empId", "empPw", "empName", "empEmail", "empBirth", "empGender", "empTel"];
+
+  /** 입력 데이터 체크 **/
   const handleDataCheck = (e) => {
     let { name, value } = e.target;
 
-    if(name === "empName") {
-      setSendData( prev => {
-        const data = { ...prev, [name]: value};
-        if(validateName(value)) setCheckData(prev => ({ ...prev, [name]: true}));
-        else setCheckData(prev => ({ ...prev, [name]: false}));
-        return data;
-      });
-    }
-
-    if(name === "empBirth") {
-      if(parseInt(value.slice(2,4)) > 12) return false;
-      if(parseInt(value.slice(4,6)) > 31) return false;
-      setSendData( prev => {
-        const data = { ...prev, [name]: value };
-        if(validateBirth(value)) setCheckData(prev => ({ ...prev, [name]: true}));
-        else setCheckData(prev => ({ ...prev, [name]: false}));
-        return data;
-      });
-
-    }
-
-    if(name === "empGender") {
-      if(value > 4) return false;
-      setSendData( prev => {
-        const data = { ...prev, [name]: value };
-        if(validateGender(value)) setCheckData(prev => ({ ...prev, [name]: true}));
-        else setCheckData(prev => ({ ...prev, [name]: false}));
+    if (name === "empTel") {
+      setSendData(prev => {
+        const data = {...prev, [name]: value};
+        if (validatePhone(data.empTel)) setCheckData(prev => ({...prev, [name]: true}));
+        else setCheckData(prev => ({...prev, [name]: false}));
         return data;
       });
     }
@@ -45,7 +29,82 @@ export const SignUpSub2 = ({ sendData, checkData,  setSendData, setCheckData }) 
         return data;
       });
     }
+
   }
+
+  const [emailCheck, setEmailCheck] = useState(false);
+  const [invalidate, setInvalidate] = useState(false);
+  const [accessNum, setAccessNum] = useState({ code: 0, input: 0});
+
+  /** E-mail 데이터 체크 **/
+  const handleCodeDataCheck = (e) => {
+    const { name, value } = e.target;
+    if(name === "code" || name === "input") setAccessNum(prev => ({ ...prev, [name]:value }));
+  }
+
+  /** 인증번호 체크 **/
+  const handleCheckCode = () => {
+    if(parseInt(accessNum.code) === parseInt(accessNum.input)) {
+      // 이메일 인증 후 인증 번호가 맞다면 아이디 요청하여 아이디 바인딩
+      setInvalidate(true);
+      alert("인증 완료");
+    }
+    else alert("인증번호 틀림");
+  }
+
+  /** 이메일로 인증번호 발송 **/
+  const handleEmailCheck = () => {
+    if(checkData.empEmail){
+      const ranNumber =  Math.floor(100000 + Math.random() * 900000);
+      console.log("ranNumber ====== ", ranNumber);
+      // 계정 조회 성공
+      // const data = {
+      //   to_name: exists.empName,
+      //   message: ranNumber
+      // }
+      // sendEmail(data);
+      setAccessNum(prev => ({ ...prev, code: ranNumber }));
+      setEmailCheck(true);
+    } else {
+      alert("E-mail을 확인하세요");
+    }
+  }
+
+  /** 회원가입 완료 버튼 이벤트 **/
+  const handleSubmit = async() => {
+    let validation = true;
+    keys.forEach(item => {
+      if(!checkData[item]) validation = false;
+    });
+
+    if(validation) {
+      let data = sendData;
+      data.empGender = data.empGender%2 === 1 ? "M" : "F";
+      delete data.pwCheck;
+      console.log(data);
+
+      const res = await axios.post(`${BaseUrl()}/employee`, sendData);
+
+      if(res.data === "ok"){
+        alert("회원가입 완료");
+        setSignUpState(false);
+      }
+
+    } else {
+      //  checkData가 fales 인 부분으로 포커스 이동하는 로직 필요
+      alert("회원가입 입력창을 확인해주세요");
+    }
+  }
+
+  const [successBtn, setSuccessBtn] = useState(false);
+  useEffect(() => {
+    let validation = true;
+    keys.forEach(item => {
+      if(!checkData[item]) validation = false;
+    });
+    if(validation) setSuccessBtn(true);
+    else setSuccessBtn(false);
+  }, [checkData]);
 
   return (
     <div className={styles.container}>
@@ -53,65 +112,39 @@ export const SignUpSub2 = ({ sendData, checkData,  setSendData, setCheckData }) 
         <h1>Join Employee!</h1>
       </div>
       <div className={styles.joinForm}>
+
         <div className={styles.row}>
-          <span>이름</span>
-          <input type="text" name="empName" onChange={handleDataCheck} value={sendData.empName || ""} placeholder="이름을 입력하세요."/>
+          <span style={sendData.empTel !== "" ? (checkData.empTel ? { color: "#37B5B6" } : { color: "red" }) : undefined} >전화번호</span>
+          <input type="text" name="empTel" onChange={handleDataCheck} value={sendData.empTel} placeholder=" ' - '를 제외한 전화번호를 입력하세요."/>
         </div>
 
-        { /* Name empty check */
-          sendData.empName !== "" ?
-            <div className={styles.row}>
-              { /* Name Check */
-                checkData.empName ?
-                    <p style={{color: "green"}}>확인되었습니다.</p>
-                    :
-                    <p style={{color: "red"}}>유효하지 않은 이름 유형입니다.</p>
-              }
-            </div>
-              : <></>
-        }
-
         <div className={styles.row}>
-          <span>생년월일</span>
-          <div>
-            <input type="text" name="empBirth" maxLength="6" value={ sendData.empBirth || "" } onChange={handleDataCheck} placeholder=""/>　-　
-            <input type="text" name="empGender" maxLength="1" value={ sendData.empGender || "" } onChange={handleDataCheck} /> ******
-          </div>
-        </div>
-
-        { /* Birth & Gender empty check */
-          sendData.empBirth !== "" && sendData.empGender !== "" ?
-            <div className={styles.row}>
-              { /* Birth & Gender check */
-                checkData.empBirth && checkData.empGender ?
-                  <p style={{color: "green"}}>확인되었습니다.</p>
-                  :
-                  <p style={{color: "red"}}>유효하지 않은 생년월일</p>
-              }
-            </div>
-              :
-            <></>
-        }
-
-        <div className={styles.row}>
-          <span>이메일</span>
+          <span style={sendData.empEmail !== "" ? (checkData.empEmail ? { color: "#37B5B6" } : { color: "red" }) : undefined} >이메일</span>
           <input type="text" name="empEmail" value={sendData.empEmail} onChange={handleDataCheck} placeholder="E-Mail을 입력하세요."/>
         </div>
+      </div>
 
-        { /* E-Mail empty check */
-          sendData.empEmail !== "" ?
-              <div className={styles.row}>
-                { /* E-Mail check */
-                  checkData.empEmail ?
-                  <p style={{color: "green"}}>확인되었습니다.</p>
-                  :
-                  <p style={{color: "red"}}>유효하지 않은 이메일</p>
-        }
+      <div className={styles.row}>
+        <button onClick={handleEmailCheck}>인증번호 받기</button>
       </div>
-      :
-      <></>
-        }
-      </div>
+
+      {
+        emailCheck &&
+        <div className={styles.row}>
+          <span style={accessNum.input !== "" ? (invalidate ? { color: "#37B5B6" } : { color: "black" }) : undefined} >인증번호</span>
+          <div>
+            <input type="text" name="input" onChange={handleCodeDataCheck} value={accessNum.input || ""} placeholder="인증번호"/>
+            <button onClick={handleCheckCode}>인증번호 입력</button>
+          </div>
+        </div>
+      }
+
+      { invalidate &&
+        <div className={styles.row}>
+          <button onClick={handleSubmit}>완료</button>
+        </div>
+      }
+
     </div>
   );
 };
