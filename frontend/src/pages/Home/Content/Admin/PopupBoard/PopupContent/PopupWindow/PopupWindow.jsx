91e59f2cb3e5e5247cf8ca80db5a2f1f07a3b142
Hayun Jeong <1116jjee@gmail.com>
2024-08-12 01:14:02 +0900

    팝업 css...

--- frontend/src/pages/Home/Content/Admin/PopupBoard/PopupContent/PopupWindow/PopupWindow.jsx
+++ frontend/src/pages/Home/Content/Admin/PopupBoard/PopupContent/PopupWindow/PopupWindow.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { BaseUrl } from '../../../../../../../commons/config';
 import { useMemberStore } from '../../../../../../../store/store';
@@ -7,7 +7,6 @@ export const PopupWindow = ({ setShowPopup }) => {
     const { sessionData } = useMemberStore();
     const openPopupIds = new Set(); // 열려 있는 팝업 ID를 저장할 Set
 
-    console.log(sessionData.empId);
     const closePop = () => {
         setShowPopup(false);
         openPopupIds.forEach(popupId => {
@@ -16,7 +15,6 @@ export const PopupWindow = ({ setShowPopup }) => {
                 popup.close(); // 모든 팝업 창 닫기
             }
         });
-        console.log('Closing all popups:', openPopupIds);
         openPopupIds.clear(); // Set 초기화
     };
 
@@ -28,6 +26,7 @@ export const PopupWindow = ({ setShowPopup }) => {
         axios.get(`${BaseUrl()}/adminpopup/today`)
             .then(resp => {
                 const announcements = resp.data;
+                let downPosition = 10;
                 let leftPosition = 10;
 
                 if (announcements.length > 0) {
@@ -51,7 +50,7 @@ export const PopupWindow = ({ setShowPopup }) => {
                             }
                         }
 
-                        const popup = window.open('', popupId, 'width=400,height=300,left=0,top=' + leftPosition + ',scrollbars=yes,resizable=no');
+                        const popup = window.open('', popupId, 'width=600,height=600,left=0'+ leftPosition+',top=' + downPosition + ',scrollbars=yes,resizable=no');
 
                         if (!popup) {
                             alert('팝업 창을 열 수 없습니다. 팝업 차단을 확인하세요.');
@@ -61,44 +60,43 @@ export const PopupWindow = ({ setShowPopup }) => {
                         // 팝업 상태 저장
                         openPopupIds.add(popupId);
                         localStorage.setItem(`${userId}_${popupId}_open`, 'true'); // 팝업이 열려 있음을 로컬 스토리지에 저장
-                        console.log('Current popups:', openPopupIds);
 
                         const content = `
                         <html>
                         <head>
                             <style>
-                                body {
-                                    font-family: 'Arial', sans-serif;
+                                @font-face {
+                                    font-family: 'ONE-Mobile-Title';
+                                    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/ONE-Mobile-Title.woff') format('woff');
+                                    font-weight: normal;
+                                    font-style: normal;
+                                }
+                                  body {
                                     margin: 0;
                                     padding: 0;
-                                    background-color: rgba(0, 0, 0, 0.5);
-                                    color: #333;
-                                    overflow: hidden;
+                                    background-color: #f0f0f0;
                                 }
                                 .popup-container {
-                                    width: 100%;
-                                    height: 100%;
-                                    display: flex;
-                                    align-items: center;
-                                    justify-content: center;
-                                    padding: 20px;
-                                }
-                                .popup-content {
-                                    width: 100%;
-                                    max-width: 500px;
-                                    background-color: #fff;
-                                    border-radius: 8px;
-                                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-                                    overflow: hidden;
                                     display: flex;
                                     flex-direction: column;
+                                    height: 100%;
+                                    width: 100%;
+                                }
+                               
+                                .popup-content {
+                                    display: flex;
+                                    flex-direction: column;
+                                    height: 90%;
+                                    overflow: hidden;
                                 }
                                 .header {
                                     padding: 20px;
-                                    border-bottom: 1px solid #eaeaea;
                                     text-align: center;
                                     background-color: #f8f9fa;
+                                    border-bottom: 1px solid #eaeaea;
                                 }
+                               
+                              
                                 h1 {
                                     font-size: 1.25em;
                                     margin: 0;
@@ -108,43 +106,30 @@ export const PopupWindow = ({ setShowPopup }) => {
                                     padding: 20px;
                                     text-align: center;
                                     line-height: 1.6;
+                                    overflow-y:auto;
                                 }
+                                 
+                                 .footer {
+                                    height: 10%;
+                                    display: flex;
+                                    justify-content: end;
+                                    align-items: center;
+                                    background-color: #f8f9fa;
+                                    border-top: 1px solid #eaeaea;
+                                }
+
                                 .checkbox-group {
                                     display: flex;
                                     justify-content: center;
                                     align-items: center;
-                                    margin: 20px 0;
+                                    margin: 0 10px;
                                 }
                                 .checkbox-group input[type="checkbox"] {
                                     margin-right: 10px;
                                 }
-                                .button-group {
-                                    display: flex;
-                                    justify-content: center;
-                                    padding: 10px;
-                                    border-top: 1px solid #eaeaea;
-                                    background-color: #f8f9fa;
-                                }
-                                .button-group button {
-                                    border: none;
-                                    padding: 10px 20px;
-                                    font-size: 1em;
-                                    cursor: pointer;
-                                    background-color: #007BFF;
-                                    color: white;
-                                    border-radius: 5px;
-                                    margin: 0 10px;
-                                    transition: background-color 0.3s ease;
-                                }
-                                .button-group button:hover {
-                                    background-color: #0056b3;
-                                }
-                                .button-group button:focus {
-                                    outline: none;
-                                    box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
-                                }
+                              
                             </style>
-                             <script>
+                            <script>
                                 let isTodayChecked = false;
                                 let isPermanentChecked = false;
 
@@ -179,6 +164,8 @@ export const PopupWindow = ({ setShowPopup }) => {
                                         window.opener.onPermanentDismiss('${popupId}');
                                     }
                                 });
+                                
+                              
                             </script>
                         </head>
                         <body>
@@ -188,17 +175,20 @@ export const PopupWindow = ({ setShowPopup }) => {
                                         <h1>${announcement.popTitle || '공지사항'}</h1>
                                     </div>
                                     <div class="content">
-                                        <div>${announcement.popContent || '내용이 없습니다.'}</div>
-                                         <div class="checkbox-group">
-                                            <input type="checkbox" id="today-checkbox" onchange="handleTodayCheckboxChange(this.checked)">
-                                            <label for="today-checkbox">오늘 하루 보지 않기</label>
-                                        </div>
-                                        <div class="checkbox-group">
-                                            <input type="checkbox" id="permanent-checkbox" onchange="handlePermanentCheckboxChange(this.checked)">
-                                            <label for="permanent-checkbox">더 이상 보지 않기</label>
+                                        <div>
+                                            ${announcement.popContent || '내용이 없습니다.'}
                                         </div>
                                     </div>
-                                    
+                                </div>
+                                <div class="footer">
+                                    <div class="checkbox-group">
+                                        <input type="checkbox" id="today-checkbox" onchange="handleTodayCheckboxChange(this.checked)">
+                                        <label for="today-checkbox">오늘 하루 보지 않기</label>
+                                    </div>
+                                    <div class="checkbox-group">
+                                        <input type="checkbox" id="permanent-checkbox" onchange="handlePermanentCheckboxChange(this.checked)">
+                                        <label for="permanent-checkbox">더 이상 보지 않기</label>
+                                    </div>
                                 </div>
                             </div>
                         </body>
@@ -208,18 +198,11 @@ export const PopupWindow = ({ setShowPopup }) => {
                         popup.document.write(content);
                         popup.document.close();
 
-                        // 팝업이 완전히 로드된 후 크기 조정
-                        popup.onload = function() {
-                            const width = Math.min(popup.document.documentElement.scrollWidth, 500); // 가로 최대 500px
-                            const height = Math.min(popup.document.documentElement.scrollHeight, 400); // 세로 최대 400px
-                            popup.resizeTo(width + 320, height + 620); // 여유 공간 추가
-                        };
-
-                        leftPosition += 50;
+                        downPosition += 50;
+                        leftPosition += 60;
                     });
 
                 } else {
-                    console.log("공지글 없을때 팝업창 닫기");
                     closePop(); 
                 }
             })
@@ -241,7 +224,6 @@ export const PopupWindow = ({ setShowPopup }) => {
         localStorage.setItem(`${userId}_${popupId}_dismissedToday`, todayDate);
         localStorage.removeItem(`${userId}_${popupId}_open`); // 팝업 상태를 제거
         openPopupIds.delete(popupId); // 팝업 ID 제거
-        console.log(`${userId}_${popupId}_dismissedToday`);
     };
 
     // '더 이상 보지 않기' 버튼 클릭 핸들러
@@ -250,6 +232,5 @@ export const PopupWindow = ({ setShowPopup }) => {
         localStorage.setItem(`${userId}_${popupId}_dismissed`, 'true');
         localStorage.removeItem(`${userId}_${popupId}_open`); // 팝업 상태를 제거
         openPopupIds.delete(popupId); // 팝업 ID 제거
-        console.log(`${userId}_${popupId}_dismissed`);
     };
 };
