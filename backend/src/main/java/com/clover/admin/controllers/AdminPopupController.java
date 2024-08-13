1792532193ea74a525efbec391d31274af7ad3a3
Hayun Jeong <1116jjee@gmail.com>
2024-08-13 18:13:40 +0900

    file 업데이트..

--- backend/src/main/java/com/clover/admin/controllers/AdminPopupController.java
+++ backend/src/main/java/com/clover/admin/controllers/AdminPopupController.java
@@ -1,6 +1,8 @@
 package com.clover.admin.controllers;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,13 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.clover.admin.dto.AdminPopupDTO;
-import com.clover.admin.dto.AdminUpdateMemDTO;
+import com.clover.admin.dto.AdminPopupUpdateDTO;
 import com.clover.admin.services.AdminPopupService;
 import com.clover.board.dto.BoardDTO;
 import com.clover.commons.dto.AttachmentDTO;
 import com.clover.commons.services.AttachmentService;
 import com.clover.commons.services.S3Service;
-import com.clover.employee.dto.EmployeeDTO;
 
 @RestController
 @RequestMapping("/adminpopup")
@@ -100,13 +101,75 @@ public class AdminPopupController {
 	}
 	
 	@PutMapping("/{popSeq}")
-	public ResponseEntity<Void> updatePopup(@PathVariable Integer popSeq, @RequestBody AdminPopupDTO popupdto){
+	public ResponseEntity<Void> updatePopup(@PathVariable Integer popSeq,@RequestBody HashMap<String, Object> data){
 		System.out.println(popSeq);
-		popupdto.setPopSeq(popSeq);
-		System.out.println(popupdto.getPopSeq());
-		adminpopupService.updatePopup(popupdto);
+		System.out.println("Request Data: " + data);
+
+		 String popTitle = (String)data.get("popTitle");
+		 String empId = (String)data.get("empId");
+		 String popContent = (String)data.get("popContent");
+		 Boolean isActive = (Boolean) data.get("popIsActive");
+	        String popIsActive = isActive.toString(); // 올바른 변환 방법
+		
+		 String periodType =(String)data.get("periodType"); // 'none', 'specific', 'monthly', 'weekly'
+	     String specificStartDate= (String)data.get("specificStartDate"); // YYYY-MM-DD
+	     String specificEndDate = (String)data.get("specificEndDate");   // YYYY-MM-DD
+	     String monthlyDay = (String)data.get("monthlyDay");       // 1-31
+	     String weeklyDay =(String)data.get("weeklyDay");      
+		AdminPopupDTO popupdto= new AdminPopupDTO(popSeq, popTitle, empId, popContent, null, null, popIsActive, null, null, periodType, specificStartDate, specificEndDate, monthlyDay, weeklyDay, null, null, null);
+		adminpopupService.updatePopup(popupdto); // 글 수정
+		
+		
+		List<String> deleteFileUrls = (List<String>) data.getOrDefault("deleteFileUrls", new ArrayList<>());
+		List<String> addFileNames = (List<String>) data.getOrDefault("addFileNames", new ArrayList<>());
+	    List<String> addFileUrls = (List<String>) data.getOrDefault("addFileUrls", new ArrayList<>());
+	    List<String> deleteImageUrls = (List<String>) data.getOrDefault("deleteImageUrls", new ArrayList<>());
+	    List<String> addImageUrls = (List<String>) data.getOrDefault("addImageUrls", new ArrayList<>());
+	    
+	    System.out.println(deleteImageUrls);
+	    System.out.println(addImageUrls);
+	    System.out.println(addFileNames);
+	    System.out.println(addFileUrls);
+	    
+	    // 삭제할 기존 파일 제거(DB, S3)
+	    if (deleteFileUrls.size() > 0) {
+			for (String deleteFileUrl : deleteFileUrls) {
+				attServ.deleteFile(deleteFileUrl);
+				s3Serv.deleteFile(deleteFileUrl);
+			}
+		}
+	    // 삭제할 기존 이미지 제거 (S3)
+	    if (deleteImageUrls.size() > 0) {
+			for (String deleteImageUrl : deleteImageUrls) {
+				s3Serv.deleteFile(deleteImageUrl);
+			}
+		}
+	    // 추가된 첨부파일이 있을 경우 
+	    if (addFileNames.size() > 0) {
+			for (int i = 0; i < addFileNames.size(); i ++) {
+				String newFilePath = "popups/" + popSeq + "/" + UUID.randomUUID() + "_" + addFileNames.get(i);
+				//파일 주소 변환 후 DB에 등록
+				String newFileUrl = s3Serv.moveFile(newFilePath, addFileUrls.get(i));
+				
+				attServ.insertFile(new AttachmentDTO(0, addFileNames.get(i), newFileUrl, "popup", popSeq));
+			}
+		}
+		// 추가된 첨부 이미지가 있을 경우
+		if (addImageUrls.size() > 0) {
+			for (int i = 0; i < addImageUrls.size(); i ++) {
+				String newImagePath = "images/popups/" + popSeq + "/" + UUID.randomUUID() + "_image" + (i+1); 
+				//이미지 주소 변환 후 글내용에서 예전 image주소들을 찾아 새로운 주소로 변환
+				String newImageUrl = s3Serv.moveFile(newImagePath, addImageUrls.get(i));
+				//변환된 주소로 글 내용을 바꾸고 반환
+				popContent = attServ.updateImageUrl(addImageUrls.get(i), newImageUrl, popContent);
+			}
+			// 최종적으로 업데이트된 글내용을 DB에 업데이트
+			attServ.updateContent(popContent, popSeq, "Popup");
+		}
+		
 		return ResponseEntity.ok().build();
 	}
+		
 }
 
 
