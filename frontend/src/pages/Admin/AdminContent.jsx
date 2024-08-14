2d14eabca19e33bf78ea4db432996bcbed5d8d29
Hayun Jeong <1116jjee@gmail.com>
2024-08-15 08:54:50 +0900

    관리자 홈 대쉬로. 팝업 체크박스 누르면 창 닫기하기로.

---
+++ frontend/src/pages/Admin/AdminContent.jsx
@@ -0,0 +1,29 @@
+import { Address } from '../Home/Content/Address/Address';
+import { Dashboard } from '../Home/Content/Admin/Dashboard/Dashboard';
+import Board from '../Home/Content/Board/Board';
+import styles from '../Home/Content/Content.module.css'
+import {Route, Routes} from "react-router-dom";
+import { ChatMain } from '../Home/Content/Messenger/ChatMain';
+import { Approval } from '../Home/Content/Approval/Approval';
+import { Member } from '../Home/Content/Admin/Member/Member';
+import { Popup } from '../Home/Content/Admin/PopupBoard/Popup';
+import { Log } from '../Home/Content/Admin/Log/Log';
+
+
+export const AdminContent = () => {
+
+  return(
+    <div className={styles.container}>
+        <Routes>
+          <Route path="/" element={ <Dashboard /> } />
+          <Route path="/address/*" element={ <Address /> } />
+          <Route path="/community/*" element={<Board/> }></Route>
+          <Route path="/chat/*" element={ <ChatMain /> } />
+          <Route path="/approval/*" element={<Approval/>}/>
+          <Route path="/member/*" element={<Member/>}/>
+          <Route path="/popup/*" element={<Popup/>}/>
+          <Route path="/log/*" element={<Log/>}/>
+        </Routes>
+    </div>
+  );
+}
