3864ff8c0fd49c207465065e985faa39b24b12ec
Sion-Noh <vmfpsel@gmail.com>
2024-08-18 22:23:50 +0900

    로딩 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import { Login } from './pages/Login/Login';
 import { SideMenu } from "./pages/SideMenu/SideMenu";
 import { Home } from "./pages/Home/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { useMemberStore } from "./store/store";
 import { useEffect, useState } from "react";
 import { AdminSideMenu } from './pages/SideMenu/AdminSideMenu';
 import { Admin } from './pages/Admin/Admin';
-import Swal from "sweetalert2";
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import {Loading} from "./components/Loading/Loading";
 
 function App() {
   const userData = sessionStorage.getItem("sessionUser");
@@ -34,29 +34,36 @@ function App() {
     if (adminCheck !== null && adminCheck === "true") {
       setAdmin(true);
     }
-  }, [sign]);
 
+    setLoading(false);
 
+  }, [sign, adminCheck]);
+
+  /** 로딩화면 추가 **/
+  const [loading, setLoading] = useState(true);
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
-    <div className="container">
-      <Router>
-      <ScrollToTop/>
-        { !sign && <Login setSign={ setSign } setAdmin={ setAdmin }/> }
-        { !admin ?
-            <>
-              { (sign && !admin) && <SideMenu open={open} setOpen={setOpen}/>  }
-              { (sign && !admin) && <Home /> }
-            </>
-            :
-            <>
-              {/* 관리자페이지 */}
-              { (sign && admin) && <AdminSideMenu open={open} setOpen={setOpen}/>  }
-              { (sign && admin) && <Admin /> }
-            </>
-        }
-      </Router>
-    </div>
+      <div className="container">
+        <Router>
+          <ScrollToTop/>
+          { !sign && <Login setSign={ setSign } setAdmin={ setAdmin }/> }
+          { !admin ?
+              <>
+                { (sign && !admin) && <SideMenu open={open} setOpen={setOpen}/>  }
+                { (sign && !admin) && <Home /> }
+              </>
+              :
+              <>
+                {/* 관리자페이지 */}
+                { (sign && admin) && <AdminSideMenu open={open} setOpen={setOpen}/>  }
+                { (sign && admin) && <Admin /> }
+              </>
+          }
+        </Router>
+      </div>
   );
 }
 
