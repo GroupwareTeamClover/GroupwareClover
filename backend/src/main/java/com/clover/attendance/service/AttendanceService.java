a00dea3207b75cf1c61da6a0bb3e34730162f285
Sion-Noh <vmfpsel@gmail.com>
2024-08-20 09:06:34 +0900

    프로젝트 title, favicon 변경

--- backend/src/main/java/com/clover/attendance/service/AttendanceService.java
+++ backend/src/main/java/com/clover/attendance/service/AttendanceService.java
@@ -3,6 +3,8 @@ package com.clover.attendance.service;
 import com.clover.attendance.dao.AttendanceDAO;
 import com.clover.attendance.dto.AttendanceDTO;
 import com.clover.commons.DateFormat;
+import com.clover.employee.dao.EmployeeDAO;
+import com.clover.employee.dto.EmployeeDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,13 @@ import java.util.List;
 import java.util.Map;
 
 @Service
-public class AttendanceServoce {
+public class AttendanceService {
 
     @Autowired
     private AttendanceDAO attendanceDAO;
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
 
     @Autowired
     private DateFormat dateFormat;
@@ -35,6 +40,26 @@ public class AttendanceServoce {
         result.put("count", attCount);
         result.put("list", attList);
         return result;
+    }
+
+    public List<Map<String, Object>> getMembersAtt(String month) {
+        Map<String, Object> map = new HashMap<>();
+        String[] range = dateFormat.getMonthRange(month);
+        map.put("start", range[0]);
+        map.put("end", range[1]);
+
+        // 전체 멤버 조회
+        List<EmployeeDTO> list = employeeDAO.getMembersInfo();
+        for (EmployeeDTO dto : list) {
+            // 멤버 별 카운트 조회
+            map.put("empSeq", dto.getEmpSeq());
+            Map<String, Object> attCount = attendanceDAO.getMyAtt(map);
+        }
+
+
+        // 총 카운트 정보
+        List<Map<String, Object>> attCount = null;
+        return attCount;
     }
 
     public AttendanceDTO todayAtt(String today, int empSeq) {
--- frontend/src/pages/Home/Content/Admin/Member/MemContent/AttendMem/AttendMem.jsx
+++ frontend/src/pages/Home/Content/Admin/Member/MemContent/AttendMem/AttendMem.jsx
@@ -1,3 +1,79 @@
-export const AttendMem =()=>{
+import styles from './AttendMem.module.css'
+import {FaSearch} from "react-icons/fa";
+import React, {useEffect} from "react";
 
+export const AttendMem = () =>{
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.date}>
+        <button> 〈 </button>
+        2024-08 근태 현황
+        <button> 〉 </button>
+      </div>
+      <div className={styles.funcBtn}>
+        <div className={styles.col_button}>
+        </div>
+        <div className={styles.searchBox}>
+          <select name="searchType" className={styles.typeBox} id="searchType">
+            <option value="0">전체부서</option>
+            <option value="1">총무</option>
+            <option value="2">인사</option>
+            <option value="3">사무</option>
+            <option value="4">유통</option>
+            <option value="5">경영</option>
+            <option value="99">미정</option>
+          </select>
+          <input type="text" id="keyword" name="keyword" autoComplete="off" placeholder="이름을 입력하세요"></input>
+          <button ><FaSearch className={styles.searchLogo}/></button>
+        </div>
+
+      </div>
+
+      <div className={styles.body}>
+        <div className={styles.tableWrapper}>
+          <table className={styles.table}>
+            <thead className={styles.thead}>
+            <tr>
+              <td className={styles.theadtd}>No.</td>
+              <td className={styles.theadtd}>부서 / 이름 / 직급</td>
+              <td className={styles.theadtd}>출근일수</td>
+              <td className={styles.theadtd}>지각</td>
+              <td className={styles.theadtd}>결석</td>
+              <td className={styles.theadtd}>연차</td>
+            </tr>
+            </thead>
+            <tbody className={styles.tbody}>
+            <tr>
+              <td className={styles.theadtd}>
+                27
+              </td>
+              <td className={styles.theadtd}>
+                총무 / 노시온 / 사장
+              </td>
+              <td className={styles.theadtd}>
+                15
+              </td>
+              <td className={styles.theadtd}>
+                3
+              </td>
+              <td className={styles.theadtd}>
+                1
+              </td>
+              <td className={styles.theadtd}>
+                1
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+
+    </div>
+  );
 }
